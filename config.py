0e38a2743093071880ef089b96fe65868bd60dff
harel moshayof <harel.moshayof@intel.com>
2023-01-12 15:43:13 +0200

    aa

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 configs={
     'word_model_path':'Embible/distilBert-20-epochs',
-    'subword_model_path':'Embible/distilBert-20-epochs',
-    'char_model_path':'../../models/Embibert-finetuned-tavbert',
+    'subword_model_path':'Embible/tavbert-20-epochs',
+    'char_model_path':'Embible/tavbert-20-epochs',
     'hf_token':'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'
 }
