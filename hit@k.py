10a57270484578456677d53d023d96134b57d4de
nivfono <54667109+nivfono@users.noreply.github.com>
2023-02-22 21:56:51 +0200

    Update hit@k.py
    working hit@k

--- hit@k.py
+++ hit@k.py
@@ -7,6 +7,26 @@ import config
 from src.ensamble import Ensamble
 from huggingface_hub import login
 
+
+
+"""
+real_values-list of words/characters
+predictions-list of lists while each list contains k words/characters
+example:
+k=2
+real_values=[שלום,ישראל]
+predictions=[[ישראל,יעקב],[חלום,ביטחון]]
+return-> 0.5
+"""
+def hit_at_k(predictions,real_values):
+    count_mone,count_mechane=0,0
+    for i,word in enumerate(real_values):
+        if word in predictions[i]:
+            count_mone+=1
+        count_mechane+=1
+    return count_mone/count_mechane
+
+
 login(config.configs['hf_token'])
 ens=Ensamble()
 with open('test.json','r') as r:
@@ -14,6 +34,15 @@ with open('test.json','r') as r:
 
 for entry in test_data:
     print(entry)
-    res=ens.predict(entry['text']).get_only_k_predictions(1)
-    print(res.get_ui_format())
+    real_values=entry['missing'].values()
+    predictions=ens.predict(entry['text']).get_only_k_predictions(5).lst#list of text parts
+    predictions=[x.predictions for x in predictions]#list of lists of predicion objects
+    list_of_preds=[]
+    for l in predictions:
+        preds=[]
+        for pred in l:
+            preds.append(pred.value)
+        list_of_preds.append(preds)
+    print(list_of_preds)
+    print(hit_at_k(list_of_preds,real_values))
     break
